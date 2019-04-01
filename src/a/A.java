c6c7a604062f852bc6149076ff48dce336358c62
gongys1 <gongys1@ifeng.com>
2019-04-01 11:51:31 +0800

    AA修改了

--- src/a/A.java
+++ src/a/A.java
@@ -4,5 +4,6 @@ public class A {
     public static void main(String[] args) {
         System.out.println("这是A");
         System.out.println("A修改了");
+        System.out.println("AA修改了");
     }
 }
